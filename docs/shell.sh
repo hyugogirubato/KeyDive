da3b1f95d957d2d10b9771ba918b9b7b7b20c833
hyugogirubato <65763543+hyugogirubato@users.noreply.github.com>
2024-04-14 19:11:26 +0200

    Update shell.sh

--- docs/shell.sh
+++ docs/shell.sh
@@ -1,19 +1,36 @@
+# Env
+export PATH='/sbin:/system/sbin:/product/bin:/apex/com.android.runtime/bin:/system/bin:/system/xbin:/odm/bin:/vendor/bin:/vendor/xbin'
 
+# Path
+export TMP='/data/local/tmp/'
+export APP='/data/data'
+export LOCAL='/storage/emulated/0'
+export WIFI='/data/misc/wifi'
+
+# Color
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 alias logcat='logcat -v color'
+alias ip='ip -c'
 
+# Custom
 alias ll='ls -alF'
 alias la='ls -A'
+
 alias l='ls -CF'
-alias ipa='ip -c a'
+alias ipa='ip a'
 alias rm='rm -rf'
 
+# Extra
 tree() {
   path=${1:-.}
   find ${path} -print | sort | sed 's;[^/]*/;|---;g;s;---|; |;g'
+}
+
+man() {
+  ${1} --help
 }
 
 clear
