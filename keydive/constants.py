cae225c3db74850f2040a14f82ff04a884cace25
hyugogirubato <65763543+hyugogirubato@users.noreply.github.com>
2025-01-10 19:13:16 +0100

    add private key func

--- keydive/constants.py
+++ keydive/constants.py
@@ -85,7 +85,7 @@ OEM_CRYPTO_API = {
     'rnmsglvj', 'polorucp', 'kqzqahjq', 'pldrclfq', 'kgaitijd', 'cwkfcplc', 'crhqcdet', 'ulns', 'dnvffnze', 'ygjiljer',
     'qbjxtubz', 'qkfrcjtw', 'rbhjspoh', 'zgtjmxko', 'igrqajte', 'ofskesua', 'qllcoacg', 'pukctkiv', 'ehdqmfmd',
     'xftzvkwx', 'gndskkuk', 'wcggmnnx', 'kaatohcz', 'ktmgdchz', 'jkcwonus', 'ehmduqyt', 'vewtuecx', 'mxrbzntq',
-    'isyowgmp'
+    'isyowgmp', 'flzfkhbc'
     # Add more as needed for different versions.
 }
 
